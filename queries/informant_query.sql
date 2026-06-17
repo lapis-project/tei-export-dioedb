495236417114660465d0f6f799960b660326362f
Jakob Bal <baljakob9@gmail.com>
2026-06-17 13:15:59 +0200

    feat: modified queries for more metadata from dioedb

--- queries/informant_query.sql
+++ queries/informant_query.sql
@@ -7,14 +7,19 @@ select
 	odto.ort_namelang,
 	odto.lat,
 	odto.lon,
-	odto.osm_id
+	odto.osm_id,
+	pdtig.gruppe_team_id,
+	pdtt.team_bez,
+	nullif(pdti.ausbildung_max, '') as ausbildung_max,
+	nullif(pdti.ausbildung_spez , '') as ausbildung_spez,
+	pdti.migrationsklasse 
 from
 	"PersonenDB_tbl_informanten" pdti
+left join "OrteDB_tbl_orte" odto on
+	odto.id = pdti.inf_ort_id 
 join "PersonenDB_tbl_personen" pdtp on
 	pdtp.id = pdti.id_person_id
 left join "PersonenDB_tbl_informantinnen_gruppe" pdtig on
 	pdtig.id = pdti.inf_gruppe_id
-left join "OrteDB_tbl_orte" odto on
-	odto.id = pdti.geburtsort_id
-where
+left join "PersonenDB_tbl_teams" pdtt on pdtt.id = pdtig.gruppe_team_id where
 	pdti.id = ANY(%s)
